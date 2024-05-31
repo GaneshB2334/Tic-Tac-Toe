5253cccd32ae7a10276d53a6857720c72cec4f8c
Ganesh Bastapure <bastapureganesh21@gmail.com>
2024-05-31 07:40:29 +0530

    Added wins counter

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ function App() {
   const [board, setBoard] = useState(Array(9).fill(''));
   const [isStart, setIsStart] = useState(false);
   const [isPlayerX, setIsPlayerX] = useState(true);
+  const [playerState, setPlayerState] = useState(isPlayerX);
   const [winner, setWinner] = useState(null);
   const [user1, setUser1] = useState('');
   const [user2, setUser2] = useState('');
+  const [countUser1, setCountUser1] = useState(0);
+  const [countUser2, setCountUser2] = useState(0);
 
   const handleClick = (i) => {
     if (board[i] || winner) {
@@ -24,6 +27,7 @@ function App() {
     } else {
       setIsPlayerX(!isPlayerX);
     }
+    console.log(playerState);
   };
 
   const checkWinner = (board) => {
@@ -40,6 +44,11 @@ function App() {
     for (let i = 0; i < matches.length; i++) {
       const [a, b, c] = matches[i];
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        if (board[a] === 'X') {
+          setCountUser1(countUser1 + 1);
+        } else {
+          setCountUser2(countUser2 + 1);
+        }
         return board[a];
       }
     }
@@ -66,15 +75,21 @@ function App() {
 
   const handleReset = () => {
     setBoard(Array(9).fill(null));
-    setIsPlayerX(true);
+    setIsPlayerX(!playerState);
     setWinner(null);
+    setPlayerState(isPlayerX);
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
       <h1 className="text-6xl m-2 text-center font-bold text-teal-500 max-md:text-3xl">Tic Tac Toe</h1>
-      {isStart ? (<> <h1 className="text-4xl m-11 text-center font-bold text-teal-500 max-md:text-lg">
-        {winner ? `Winner is ${winner === 'X' ? user1 : user2}` : `Next Player is ${isPlayerX ? user1 : user2}`}
+      {isStart && <div className='text-xl font-semibold flex flex-col justify-center items-center m-5 text-teal-500'>
+        <p>Winnings</p>
+        <p>{user1 ? user1 : "X"}: {countUser1}</p>
+        <p>{user2 ? user2 : 'O'}: {countUser2}</p>
+      </div>}
+      {isStart ? (<> <h1 className="text-4xl m-5 text-center font-bold text-teal-500 max-md:text-lg">
+        {winner ? `Winner is ${winner === 'X' ? user1 ? user1 : 'X' : user2 ? user2 : 'O'}` : `Next Player is ${isPlayerX ? user1 != '' ? user1 : 'X' : user2 != '' ? user2 : 'O'}`}
       </h1>
         <div className="flex flex-col items-center gap-5">
           <div className="flex gap-5">
@@ -93,7 +108,7 @@ function App() {
             {renderBox(8)}
           </div>
           <button
-            className="bg-blue-400 p-5 text-center w-32 text-xl font-semibold rounded-full shadow-2xl shadow-black hover:scale-105 transition-all max-md:w-24 max-md:p-3 max-md:text-sm"
+            className="bg-blue-400 p-5 text-center w-32 text-xl font-semibold rounded-full shadow-2xl shadow-black hover:scale-105 transition-all max-md:w-24 max-md:p-3 max-md:text-sm mb-10"
             onClick={handleReset}
           >
             Reset
@@ -103,17 +118,17 @@ function App() {
         <div className="flex flex-col items-center justify-center gap-10 p-3">
           <input
             type="text"
-            placeholder="Enter Player 1 Name"
             className="p-5 text-center w-64 text-xl font-semibold rounded-full shadow-2xl shadow-black border-dashed border-2 border-teal-800 outline-none max-md:p-2 max-md:w-52"
             value={user1}
             onChange={(e) => setUser1(e.target.value)}
+            placeholder="Enter Player 1 Name"
           />
           <input
             type="text"
-            placeholder="Enter Player 2 Name"
             className="p-5 text-center w-64 text-xl font-semibold rounded-full shadow-2xl shadow-black border-dashed border-2 border-teal-800 outline-none max-md:p-2 max-md:w-52"
             value={user2}
             onChange={(e) => setUser2(e.target.value)}
+            placeholder="Enter Player 2 Name"
           />
           <button
             className="bg-blue-400 p-5 text-center w-32 text-xl font-semibold rounded-full shadow-2xl shadow-black hover:scale-105 transition-all max-md:w-24 max-md:p-3 max-md:text-sm"
